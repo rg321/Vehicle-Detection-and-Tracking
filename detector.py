b05aaa99e7e48ab5c684fbbea8fa94e9181515bc
raghavG <guptaraghu321@gmail.com>
2021-03-17 16:36:55 +0530

    changes to run on colab, to run on tensorflowv2

--- detector.py
+++ detector.py
@@ -3,7 +3,9 @@ Implement and test car detection (localization)
 '''
 
 import numpy as np
-import tensorflow as tf
+# import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 from PIL import Image
 import os
 from matplotlib import pyplot as plt
